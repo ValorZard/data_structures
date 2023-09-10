384dc99c05577add5d50fc4b6cf8c0cddc504b5e
Srayan Jana <valorzard.kiko@outlook.com>
2023-09-09 18:50:47 -0700

    importing files does not go well

--- DataStructuresTest/test.cpp
+++ DataStructuresTest/test.cpp
@@ -1,7 +1,8 @@
 #include "pch.h"
 #include "../data_structures/array_list.h"
 #include "../data_structures/sorting.h"
-
+//#include "test_linked_list.h"
+//#include "test_binary_tree.h"
 
 int main(int argc, char** argv)
 {
